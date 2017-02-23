d9ff9811894e43b008f41ec6fe7ff9d76a094d55
Stefan Krstic <stefan@stuntcoders.com>
2017-02-23 17:00:26 +0100

    Add managed stock check in system config

--- app/code/local/Stuntcoders/GoogleShopping/Helper/Data.php
+++ app/code/local/Stuntcoders/GoogleShopping/Helper/Data.php
@@ -2,21 +2,29 @@
 
 class Stuntcoders_GoogleShopping_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const MANAGED_STOCK_CONFIG_PATH = 'stuntcoders_googleshopping/additional_options/managed_stock';
+
     public function getCategoriesOptions()
     {
         $categories = Mage::getModel('catalog/category')->getCollection()
             ->addAttributeToSelect('name')
-            ->addAttributeToFilter('name', array('neq' => ''));
+            ->addAttributeToFilter('name', array('neq' => ''))
+            ->setOrder('path', Varien_Data_Collection_Db::SORT_ORDER_ASC);
 
         $values = array();
 
         foreach ($categories as $category) {
             $values[] = array(
                 'value' => $category->getId(),
-                'label' => $category->getName(),
+                'label' => str_repeat('––', $category->getLevel()) . ' ' . $category->getName(),
             );
         }
 
         return $values;
+    }
+
+    public function isStockManaged()
+    {
+        return Mage::getStoreConfigFlag(self::MANAGED_STOCK_CONFIG_PATH);
     }
 }
