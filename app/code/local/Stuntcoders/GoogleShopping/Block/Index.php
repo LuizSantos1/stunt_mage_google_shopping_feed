d9142f420fc42a0cf2f900da5abd9df9bcbe3327
Stefan Krstic <stefan@stuntcoders.com>
2017-02-23 16:52:12 +0100

    Refactor Index Block prepareLayout function

--- app/code/local/Stuntcoders/GoogleShopping/Block/Index.php
+++ app/code/local/Stuntcoders/GoogleShopping/Block/Index.php
@@ -4,12 +4,13 @@ class Stuntcoders_GoogleShopping_Block_Index extends Mage_Adminhtml_Block_Widget
 {
     protected function _prepareLayout()
     {
-        $this->setChild('googleshopping.addnew',
-            $this->getLayout()->createBlock('adminhtml/widget_button')->setData(array(
-                'label' => Mage::helper('stuntcoders_googleshopping')->__('Add New Feed'),
-                'onclick' => "setLocation('".$this->getUrl('*/*/add')."')",
-                'class' => 'add'
-            ))
-        );
+        $addButton = $this->getLayout()->createBlock('adminhtml/widget_button');
+        $addButton ->setData(array(
+            'label' => Mage::helper('stuntcoders_googleshopping')->__('Add New Feed'),
+            'onclick' => "setLocation('" . $this->getUrl('*/*/add') . "')",
+            'class' => 'add'
+        ));
+
+        $this->setChild('googleshopping.addnew', $addButton);
     }
 }
