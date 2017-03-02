9fc7925f37c07de45323fd39e793d8b33d292a95
Mladen Ilic <mladen@stuntcoders.com>
2017-03-02 20:06:47 +0100

    Do not load feed model twice

--- app/code/local/Stuntcoders/GoogleShopping/Model/Observer.php
+++ app/code/local/Stuntcoders/GoogleShopping/Model/Observer.php
@@ -2,16 +2,14 @@
 
 class Stuntcoders_GoogleShopping_Model_Observer
 {
-    public function setFeeds()
+    public function generateFeeds()
     {
         $feeds = Mage::getModel('stuntcoders_googleshopping/feed')->getCollection();
-
         foreach ($feeds as $feed) {
-            $feedModel = Mage::getModel('stuntcoders_googleshopping/feed')->load($feed->getId());
-
+            /** @var Stuntcoders_GoogleShopping_Model_Feed $feed */
             $file = new Varien_Io_File();
             $file->mkdir(dirname($feed->getPath()), 755, true);
-            $file->write($feed->getPath(), $feedModel->generateXml());
+            $file->write($feed->getPath(), $feed->generateXml());
         }
     }
 }
